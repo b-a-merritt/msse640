a8e3810c5da3e87b9a047f6291eb3e31dec00d5a
Ben <ben.a.merritt@gmail.com>
2025-08-16 16:01:03 +0300

    add chrome version fix to ci

--- week8/project4/conftest.py
+++ week8/project4/conftest.py
@@ -14,7 +14,6 @@ def chrome_browser():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
 
     driver = webdriver.Chrome(options=chrome_options)
