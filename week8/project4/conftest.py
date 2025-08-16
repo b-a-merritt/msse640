23ba205caf1c2dba01ca4a193291b2961c17f8ee
Ben <ben.a.merritt@gmail.com>
2025-08-16 14:57:00 +0300

    fix acceptance tests by running headless

--- week8/project4/conftest.py
+++ week8/project4/conftest.py
@@ -1,5 +1,10 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption("--headless", action="store_true", help="Run browser in headless mode")
 
 
 def pytest_configure(config):
@@ -9,8 +14,15 @@ def pytest_configure(config):
     )
 
 @pytest.fixture()
-def chrome_browser():
-    driver = webdriver.Chrome()
+def chrome_browser(request):
+    chrome_options = Options()
+    if request.config.getoption("--headless"):
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")
+
+    driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(5)
 
     yield driver
